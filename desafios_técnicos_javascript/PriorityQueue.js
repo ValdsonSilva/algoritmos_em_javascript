f8417646be2fb27d1b238f7083c274a923b604c5
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2024-07-27 13:03:22 -0300

    Desafio da fila com prioridades finalizado

--- desafios_técnicos_javascript/PriorityQueue.js
+++ desafios_técnicos_javascript/PriorityQueue.js
@@ -33,33 +33,43 @@ class PriorityQueue {
 
     // remove e retorna o eleemento com maior prioridade da fila
     dequeue() {
-    
         var element_with_priority = this.front()
         console.log("Elemento de maior prioridade: ", element_with_priority)
-        console.log(this.elements.filter((arr) => arr !== element_with_priority))
+        // retorna todos os elementos que sejam diferentes do elemento passado na condição
+        console.log(this.elements.filter((element) => element !== element_with_priority))
         // return `Lista atual ${this.elements} e o elemento removido ${element_with_priority}`
 
     }   
 
-    print() {
+    // verifica se a fila está vazia
+    isEmpty() {
+        if (this.elements) {
+            return `A fila não tá fazia ${true}`
+        } else {
+            return `A fila não está fazia ${false}`
+        }
+    }
+
+    // Retorna o tamanho da fila
+    size() {
+        return `O tamanho da fila ${this.elements.length}`
+    }
+
+    printQueue() {
         console.log(`Fila de prioridades: ${this.elements.map((item) => `\n{\n\telement:${item.element},\n\tpriority:${item.priority}\n}`)}`)
     }
 }
 
 
 // intância da minha classe
-const obj = new PriorityQueue()
-const tam = 10
+const pq = new PriorityQueue();
 
-for (let i = 1; i <= tam; i++) {
-    obj.enqueue(3 * Math.random(), 2 * (Math.random()/(2)))
-}
+pq.enqueue("Tarefa 1", 2);
+pq.enqueue("Tarefa 2", 1);
+pq.enqueue("Tarefa 3", 3);
 
-obj.print()
-
-console.log(obj.front())
-
-// console.log(obj.dequeue())
-obj.dequeue()
-
-// obj.print()
+console.log(pq.printQueue()); // [ { element: 'Tarefa 2', priority: 1 }, { element: 'Tarefa 1', priority: 2 }, { element: 'Tarefa 3', priority: 3 } ]
+console.log(pq.dequeue()); // { element: 'Tarefa 2', priority: 1 }
+console.log(pq.front()); // { element: 'Tarefa 1', priority: 2 }
+console.log(pq.isEmpty()); // false
+console.log(pq.size()); // 2
